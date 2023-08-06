23953e0b013ebbc2ce178f3d195766d26dd8afe6
yazn_108 <102805404+yazn-108@users.noreply.github.com>
2023-08-06 03:22:47 +0300

    ToDo List

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ lettersAndSpace.forEach(letter => {
     letter === ' '? span.className = "space": false;
     lettersGuess.appendChild(span);
 });
+let keysArray = [];
 let wrongAttempts = 0;
 let draw = document.querySelector(".hangman-draw")
 let guessSpan = document.querySelectorAll(".letters-guess span");
@@ -35,6 +36,7 @@ Array.from(document.querySelectorAll(".letter-box")).forEach((e) => {
         let Status = false;
         e.target.classList.add("clicked");
         let clickedLetter = e.target.innerHTML.toLowerCase();
+        keysArray.push(clickedLetter)
         let theChosenWord  = Array.from(randomValue.toLowerCase())
         theChosenWord.forEach((wordLetter,WordIndex) => {
             if (wordLetter == clickedLetter) {
@@ -84,12 +86,86 @@ Array.from(document.querySelectorAll(".letter-box")).forEach((e) => {
             wrong.textContent = `wrong attempts: ${wrongAttempts}`;
             winMessage.appendChild(wrong);
             let button = document.createElement("button");
-                button.textContent = "next";
-                button.className = "next"
+            button.textContent = "next";
+            button.className = "next"
             winMessage.appendChild(button);
             document.body.appendChild(winMessage);
         };
     });
+});
+document.addEventListener('keypress', function(event) {
+    event.key === 'Enter'?event.preventDefault():"";
+    let found = false;
+    for (let i = 0; i < keysArray.length; i++) {
+        if (keysArray[i] === event.key.toLowerCase()){
+            found = true;
+        };
+    };
+    if (found === false){
+        keysArray.push(event.key);
+        let Status = false;
+        var pressedKey = event.key.toLowerCase();
+        Array.from(document.querySelectorAll(".letter-box")).forEach(function(letter) {
+            if (pressedKey === letter.innerHTML.toLowerCase()) {
+                letter.classList.add("clicked");
+                let clickedLetter = letter.innerHTML.toLowerCase();
+                let theChosenWord  = Array.from(randomValue.toLowerCase())
+                theChosenWord.forEach((wordLetter,WordIndex) => {
+                    if (wordLetter == clickedLetter) {
+                        Status = true;
+                        guessSpan.forEach((span,spanIndex) => {
+                            if (WordIndex === spanIndex) {
+                                span.innerHTML = clickedLetter;
+                            };
+                        });
+                    };
+                });
+                if (Status === false) {
+                    wrongAttempts++;
+                    draw.classList.add(`wrong-${wrongAttempts}`);
+                    if (wrongAttempts === 9) {
+                        lettersContainer.classList.add("finished");
+                        let lossMessage = document.createElement("div");
+                        lossMessage.className = "message";
+                        let p = document.createElement("p");
+                        p.textContent = 'game over, the word is:'
+                        let span = document.createElement("span");
+                        span.textContent = randomValue
+                        let button = document.createElement("button");
+                        button.textContent = "next";
+                        button.className = "next";
+                        lossMessage.appendChild(p);
+                        lossMessage.appendChild(span);
+                        lossMessage.appendChild(button);
+                        setTimeout(() => {
+                            document.body.appendChild(lossMessage);
+                        }, 500);
+                    };
+                };
+            };
+        });
+        let allLettersGuessed = true;
+        guessSpan.forEach((span) => {
+            if (span.innerHTML === "") {
+                allLettersGuessed = false;
+            };
+        });
+        if (allLettersGuessed === true && event.key !== "Enter") {
+            let winMessage = document.createElement("div");
+            winMessage.className = "message";
+            let p = document.createElement("p");
+            p.textContent = "Congratulations, you won!";
+            winMessage.appendChild(p);
+            let wrong = document.createElement("p");
+            wrong.textContent = `wrong attempts: ${wrongAttempts}`;
+            winMessage.appendChild(wrong);
+            let button = document.createElement("button");
+            button.textContent = "next";
+            button.className = "next"
+            winMessage.appendChild(button);
+            document.body.appendChild(winMessage);
+        };
+    };
 });
 document.addEventListener("click",(e) =>{
     e.target.classList.contains("next")?
